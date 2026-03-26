13f34ea9a558d6ebaa3a5e1632ebef0f015eabed
Brook Li <emcdynamicist@gmail.com>
2026-03-27 01:49:07 +0900

    Init commmit with Prettier set up and a media query hook written

---
+++ src/useDarkModePref.ts
@@ -0,0 +1,18 @@
+import {useSyncExternalStore} from 'react';
+
+const darkModeQueryList = window.matchMedia('(prefers-color-scheme: dark)')
+
+export default function useDarkModePref() {
+    return useSyncExternalStore(subscribe, getSnapshot);
+}
+
+function getSnapshot() {
+    return darkModeQueryList.matches;
+}
+
+function subscribe(callback: (this: MediaQueryList, ev: MediaQueryListEvent) => void) {
+    darkModeQueryList.addEventListener('change', callback);
+    return () => {
+        darkModeQueryList.removeEventListener('change', callback);
+    };
+}
